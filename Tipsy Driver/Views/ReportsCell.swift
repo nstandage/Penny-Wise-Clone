bc785d20924dd7aa1f72245042927f8430809b7f
Nathan Standage <developer@nathanstandage.com>
2018-08-03 16:06:46 -0600

    This is 2.1 Added Reports screen which lists all entries and also has a button for CSV exporting.

---
+++ Tipsy Driver/Views/ReportsCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReportsCell.swift
+//  Tipsy Driver
+//
+//  Created by Nathan Standage on 8/3/18.
+//  Copyright © 2018 Nathan Standage. All rights reserved.
+//
+
+import UIKit
+
+class ReportsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
